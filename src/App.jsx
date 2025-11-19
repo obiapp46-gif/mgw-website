a54bece9a2940d3c737858877c5188eb6c7f7def
Mark Gibbens <mark@example.com>
2025-11-19 15:21:29 +0000

    Fix routing and privacy path

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import CastScan from "./pages/CastScan";
-import PartsReference from "./pages/PartsReference";
 
-<Routes>
-  ...
-  <Route path="/castscan" element={<CastScan />} />
-  <Route path="/parts-reference" element={<PartsReference />} />
-</Routes>
-
-
+// Pages
 import Home from "./pages/Home";
 import Features from "./pages/Features";
 import Download from "./pages/Download";
 import Support from "./pages/Support";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
+import CastScan from "./pages/CastScan";
+import PartsReference from "./pages/PartsReference";
 
 export default function App() {
   return (
@@ -31,8 +25,14 @@ export default function App() {
             <Route path="/features" element={<Features />} />
             <Route path="/download" element={<Download />} />
             <Route path="/support" element={<Support />} />
+
+            {/* Important legal pages */}
             <Route path="/privacy" element={<Privacy />} />
             <Route path="/terms" element={<Terms />} />
+
+            {/* New pages */}
+            <Route path="/castscan" element={<CastScan />} />
+            <Route path="/parts-reference" element={<PartsReference />} />
           </Routes>
         </main>
 
